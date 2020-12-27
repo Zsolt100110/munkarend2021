e3f83d42c9a256d43dd4c45732e9337ab1371e5b
Zsolt100110 <76620012+Zsolt100110@users.noreply.github.com>
2020-12-27 17:49:17 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,194 @@
+function msz1() {
+  document.getElementById("active1").innerHTML = "MSZ";
+  setTimeout("setToOriginal1()", 3000);
+}
+function setToOriginal1() {
+  document.getElementById("active1").innerHTML = "1" ; //jan.1
+}
+
+
+function msz2() {
+  document.getElementById("active2").innerHTML = "MSZ";
+  setTimeout("setToOriginal2()", 3000);
+}
+function setToOriginal2() {
+  document.getElementById("active2").innerHTML = "1" ; //maj.1
+}
+
+
+function msz3() {
+  document.getElementById("active3").innerHTML = "MSZ";
+  setTimeout("setToOriginal3()", 3000);
+}
+function setToOriginal3() {
+  document.getElementById("active3").innerHTML = "15" ; //marc.15
+}
+
+function msz4() {
+  document.getElementById("active4").innerHTML = "MSZ";
+  setTimeout("setToOriginal4()", 3000);
+}
+function setToOriginal4() {
+  document.getElementById("active4").innerHTML = "2" ; //apr.2
+}
+
+function msz5() {
+  document.getElementById("active5").innerHTML = "MSZ";
+  setTimeout("setToOriginal5()", 3000);
+}
+function setToOriginal5() {
+  document.getElementById("active5").innerHTML = "4" ; //apr.4
+}
+
+
+function msz6() {
+  document.getElementById("active6").innerHTML = "MSZ";
+  setTimeout("setToOriginal6()", 3000);
+}
+function setToOriginal6() {
+  document.getElementById("active6").innerHTML = "5" ; //apr.5
+}
+
+function msz7() {
+  document.getElementById("active7").innerHTML = "MSZ";
+  setTimeout("setToOriginal7()", 3000);
+}
+function setToOriginal7() {
+  document.getElementById("active7").innerHTML = "23" ; //maj.23
+}
+
+
+function msz8() {
+  document.getElementById("active8").innerHTML = "MSZ";
+  setTimeout("setToOriginal8()", 3000);
+}
+function setToOriginal8() {
+  document.getElementById("active8").innerHTML = "24" ; //maj.24
+}
+
+function msz9() {
+  document.getElementById("active9").innerHTML = "MSZ";
+  setTimeout("setToOriginal9()", 3000);
+}
+function setToOriginal9() {
+  document.getElementById("active9").innerHTML = "20" ; //aug.20
+}
+
+function msz10() {
+  document.getElementById("active10").innerHTML = "MSZ";
+  setTimeout("setToOriginal10()", 3000);
+}
+function setToOriginal10() {
+  document.getElementById("active10").innerHTML = "23" ; //okt.23
+}
+
+function msz11() {
+  document.getElementById("active11").innerHTML = "MSZ";
+  setTimeout("setToOriginal11()", 3000);
+}
+function setToOriginal11() {
+  document.getElementById("active11").innerHTML = "1" ; //nov.1
+}
+
+function msz12() {
+  document.getElementById("active12").innerHTML = "MSZ";
+  setTimeout("setToOriginal12()", 3000);
+}
+function setToOriginal12() {
+  document.getElementById("active12").innerHTML = "25" ; //dec.25
+}
+
+function msz14() {
+  document.getElementById("active14").innerHTML = "MSZ";
+  setTimeout("setToOriginal14()", 3000);
+}
+function setToOriginal14() {
+  document.getElementById("active14").innerHTML = "26" ; //dec.26
+}
+
+//-------------------------------------------------------------------------------------
+function day20() {
+  document.getElementById("active20").innerHTML = "-11,5";
+  setTimeout("setToOriginal20()", 3000);
+}
+function setToOriginal20() {
+  document.getElementById("active20").innerHTML = "14" ; //marc.14 -11,5
+}
+
+function day21() {
+  document.getElementById("active21").innerHTML = "-8";
+  setTimeout("setToOriginal21()", 3000);
+}
+function setToOriginal21() {
+  document.getElementById("active21").innerHTML = "3" ; //apr.3 -8
+}
+
+function day22() {
+  document.getElementById("active22").innerHTML = "-11,5";
+  setTimeout("setToOriginal22()", 3000);
+}
+function setToOriginal22() {
+  document.getElementById("active22").innerHTML = "30" ; //apr.30 -11,5
+}
+
+
+function day23() {
+  document.getElementById("active23").innerHTML = "-8";
+  setTimeout("setToOriginal23()", 3000);
+}
+function setToOriginal23() {
+  document.getElementById("active23").innerHTML = "22" ; //maj.22 -8
+}
+
+
+function day24() {
+  document.getElementById("active24").innerHTML = "-11,5";
+  setTimeout("setToOriginal24()", 3000);
+}
+function setToOriginal24() {
+  document.getElementById("active24").innerHTML = "19" ; //aug.19 -11,5
+}
+
+
+function day25() {
+  document.getElementById("active25").innerHTML = "-11,5";
+  setTimeout("setToOriginal25()", 3000);
+}
+function setToOriginal25() {
+  document.getElementById("active25").innerHTML = "19" ; //dec.19 -11,5
+}
+
+
+
+
+function day27() {
+  document.getElementById("active27").innerHTML = "-8";
+  setTimeout("setToOriginal27()", 3000);
+}
+function setToOriginal27() {
+  document.getElementById("active27").innerHTML = "27" ; //dec.27 -8
+}
+
+
+
+
+function day28() {
+  document.getElementById("active28").innerHTML = "-8";
+  setTimeout("setToOriginal28()", 3000);
+}
+function setToOriginal28() {
+  document.getElementById("active28").innerHTML = "28" ; //dec.28 -8
+}
+
+
+
+
+function day29() {
+  document.getElementById("active29").innerHTML = "-11,5";
+  setTimeout("setToOriginal29()", 3000);
+}
+function setToOriginal29() {
+  document.getElementById("active29").innerHTML = "29" ; //dec.29 -11,5
+}
+//---------------------------------------------------------------------------------
+
